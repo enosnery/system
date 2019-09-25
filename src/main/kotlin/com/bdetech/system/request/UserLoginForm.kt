61cf4e9bb7317c2688451c00a924094e532dfd21
enosnery <enosnery84@gmail.com>
2019-09-25 16:52:40 -0300

    Remoção de override do toString

--- src/main/kotlin/com/bdetech/system/request/UserLoginForm.kt
+++ src/main/kotlin/com/bdetech/system/request/UserLoginForm.kt
@@ -3,8 +3,4 @@ package com.bdetech.system.request
 class UserLoginForm(
         val login : String,
         val password: String
-){
-    override fun toString(): String {
-        return "UserLoginForm(login='$login', password='$password')"
-    }
-}
+)
